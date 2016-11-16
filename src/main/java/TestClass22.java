b51abd146dda63edb24fe3bdfb720fa6a4448aeb
Dr.Mokter <mokter@gmail.com>
2016-11-16 14:32:32 -0600

    Test Class22 edited second time"

--- src/main/java/TestClass22.java
+++ src/main/java/TestClass22.java
@@ -3,7 +3,7 @@ public class TestClass22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Test Class22 added  and edited ");
+ System.out.println("Test Class22 added  and edited second time");
 	}
 
 }
